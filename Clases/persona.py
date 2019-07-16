7f60134b48a8864dbd98511da4d0b85b18fe2836
Jose Ocampos <jose.ocampos.1618@gmail.com>
2019-07-16 16:46:19 -0400

    Creacion de metodos para la clase Persona

--- Clases/persona.py
+++ Clases/persona.py
@@ -1,19 +1,28 @@
-class Persona:
-    edad = 0
+class Persona:      # Definimos la clase, una receta para crear un objeto
+                    # o también la clase es pla "plantilla"
+    edad = 0        # Atributo de clase o propiedad del objeto que vamos a crear
 
-    def __init__(self, un_nombre):
-        self.mi_nombre = un_nombre
+    def __init__(self, un_nombre):  #__init__ es el método constructor
+                                    # métodos --> funciones dentro de una clase
+        self.mi_nombre = un_nombre  # Usamos self para referirnos al objeto mismo
         print("Hola, nací, me llamo", self.mi_nombre)
     
-    def cumple(self):
-        self.edad = self.edad + 1
+    def cumple(self):               # Cumple es un método de la clase Persona
+        self.edad = self.edad + 1   # que aumenta la propiedad edad en 1
+
+    def asignar_edad(self,mi_edad): # Es un método que recibe un argumento
+        self.edad = mi_edad         # y asigna a la propiedad edad del objeto
+        print("Mi edad es", self.edad, "años")
     
     def fav_color(self,color):
         self.color_favorito = color
         print("Mi color favorito es", self.color_favorito)
 
-pepe = Persona("Pepito")
-pepe.cumple()
+    def altura(self,altura_cm):
+        self.mi_altura = altura_cm
+        print("Mido", self.mi_altura,"cm")
 
-print("Acabo de cumplir", pepe.edad)
+pepe = Persona("Pepito")
+pepe.asignar_edad(22)
 pepe.fav_color("azul")
+pepe.altura(170)

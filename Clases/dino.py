00847afbf7a584b8162618de119fa94fa48e00ea
Jose Ocampos <jose.ocampos.1618@gmail.com>
2019-07-17 15:20:06 -0400

    Se agregaron ejemplos de herencia

--- Clases/dino.py
+++ Clases/dino.py
@@ -1,4 +1,16 @@
-class Dino:
+class Animal:
+    vivo = False
+
+    def __init__(self):
+        self.vivo = True
+
+    def esta_vivo(self):
+        print("Estoy vivo")
+    
+    def morir(self):
+        self.vivo = False
+
+class Dino(Animal):
     def __init__(self, un_nombre, un_color, canti_patas=2, un_genero=None):
         self.nombre = un_nombre
         self.color = un_color
@@ -23,3 +35,14 @@ rex.saludar()
 print(rex.patas)
 rex.cortar_pata(3)
 print(rex.patas)
+
+class TRex(Dino):
+
+    def __init__(self, nombre, patas = 4, color = None):
+        self.nombre = nombre
+        self.patas = patas
+        self.color = color
+        print("Hola, soy un TRex y me llamo", self.nombre)
+
+roberto = TRex("Roberto",4)
+print(roberto.vivo)
